29793ab5bfc1af3d87731f981f419bb47a2ebe49
Angel Castro Desktop-Home <evivaly9@hotmail.com>
2023-05-29 15:39:39 -0500

    dag load live -endpoint

---
+++ DAGS/load_live_url.py
@@ -0,0 +1,40 @@
+
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner' : 'angel',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id='dgraph_insert_live_url',
+    default_args = default_args,
+    description='this is my first dag',
+    start_date=datetime(2023,5,12,2),
+    schedule_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command= "echo hello Angel, this is task 1"
+    )
+
+    task2 = BashOperator(
+        task_id = 'second_task',
+        bash_command= "dgraph live --slash_grpc_endpoint <grpc-endpoint> -f <local-path-to-data>/<data-file> -s <local-path-to-data>/<schema-file> -t <api-key>"
+        
+
+    )
+
+    task3 = BashOperator(
+        task_id = 'third_task',
+        
+        bash_command = "curl -H 'Content-Type: application/dql' -X POST http://44.211.75.97:8000/query -d $' { movies(func: has(release_date)) { name,  director { name },    starring { name } \
+     } }'"
+    )
+
+    task1.set_downstream(task2)
+    task1.set_downstream(task3)
