d14b60bbc313d5989f135a3ac017666a6c27927f
ACSR <andysanru@gmail.com>
2023-06-23 22:13:55 -0500

    :sparkles: load five rdf zip files

--- DAGS/dag_dgraph_load_rdf_file.py
+++ DAGS/dag_dgraph_load_rdf_file.py
@@ -12,40 +12,80 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def load_rdf_file_a(ti):
-    ti.xcom_push(key='name', value='Mycelium')
-    url ="http://34.170.231.213:8080/mutate?commitNow=true"
-    headers={
-        'Accept':'*/*',
-        'Content-Encoding':'gzip',
-        'Content-Type':'application/rdf',
-        'Accept-Encoding':'gzip, deflate'
-    }
+url ="http://34.170.231.213:8080/mutate?commitNow=true"
+headers={
+    'Accept':'*/*',
+    'Content-Encoding':'gzip',
+    'Content-Type':'application/rdf',
+    'Accept-Encoding':'gzip, deflate'
+}
 
-    dataFileName = os.path.join(conf.get('core', 'DAGS_FOLDER'), 'data/accountRelations_1_1.rdf.gz')
+def load_rdf_file_a(ti):
+    global url, headers
+    fileId = "1_1"
+
+    fileName = "accountRelations_"+fileId+".rdf.gz"
+    ti.xcom_push(key='name', value=fileName)
+
+    dataFileName = os.path.join(conf.get('core', 'DAGS_FOLDER'), 'data/accountRelations_'+fileId+'.rdf.gz')
     with open(dataFileName, 'rb') as dataRaw:
         resp = requests.post(url,headers=headers, data=dataRaw)
     
 def load_rdf_file_b(ti):
-    ti.xcom_push(key='name', value='Mycelium')
-    url ="http://34.170.231.213:8080/mutate?commitNow=true"
-    headers={
-        'Accept':'*/*',
-        'Content-Encoding':'gzip',
-        'Content-Type':'application/rdf',
-        'Accept-Encoding':'gzip, deflate'
-    }
+    global url, headers
+    fileId = "1_2"
 
-    dataFileName = os.path.join(conf.get('core', 'DAGS_FOLDER'), 'data/accountRelations_1_2.rdf.gz')
+    fileName = "accountRelations_"+fileId+".rdf.gz"
+    ti.xcom_push(key='name', value=fileName)
+
+    dataFileName = os.path.join(conf.get('core', 'DAGS_FOLDER'), 'data/accountRelations_'+fileId+'.rdf.gz')
     with open(dataFileName, 'rb') as dataRaw:
         resp = requests.post(url,headers=headers, data=dataRaw)
+        ti.xcom_push(key='status', value=str(resp.status_code))
+        
+def load_rdf_file_c(ti):
+    global url, headers
+    fileId = "1_3"
 
+    fileName = "accountRelations_"+fileId+".rdf.gz"
+    ti.xcom_push(key='name', value=fileName)
+
+    dataFileName = os.path.join(conf.get('core', 'DAGS_FOLDER'), 'data/accountRelations_'+fileId+'.rdf.gz')
+    with open(dataFileName, 'rb') as dataRaw:
+        resp = requests.post(url,headers=headers, data=dataRaw)
+        ti.xcom_push(key='status', value=str(resp.status_code))
+        
+def load_rdf_file_d(ti):
+    global url, headers
+    fileId = "1_4"
+
+    fileName = "accountRelations_"+fileId+".rdf.gz"
+    ti.xcom_push(key='name', value=fileName)
+
+    dataFileName = os.path.join(conf.get('core', 'DAGS_FOLDER'), 'data/accountRelations_'+fileId+'.rdf.gz')
+    with open(dataFileName, 'rb') as dataRaw:
+        resp = requests.post(url,headers=headers, data=dataRaw)
+        ti.xcom_push(key='status', value=str(resp.status_code))
+        
+def load_rdf_file_e(ti):
+    global url, headers
+    fileId = "1_5"
+
+    fileName = "accountRelations_"+fileId+".rdf.gz"
+    ti.xcom_push(key='name', value=fileName)
+
+    dataFileName = os.path.join(conf.get('core', 'DAGS_FOLDER'), 'data/accountRelations_'+fileId+'.rdf.gz')
+    with open(dataFileName, 'rb') as dataRaw:
+        resp = requests.post(url,headers=headers, data=dataRaw)
+        ti.xcom_push(key='status', value=str(resp.status_code))
+        
 def log_print_py(ti):
     name = ti.xcom_pull(task_ids = 'load_rdf_gz_a', key='name')
+    ti.xcom_push(key='status', value=name)
     print(f'hello world {name}')
 
 with DAG(
-    dag_id= 'dgraph_load_rdf_file_r',
+    dag_id= 'dgraph_load_rdf_file_s',
     default_args=default_args,
     description='dgraph load data',
     start_date=datetime(2023,6,15),
@@ -58,12 +98,27 @@ with DAG(
 
     task2 = PythonOperator(
         task_id='load_rdf_gz_b',
-        python_callable=load_rdf_file_a
+        python_callable=load_rdf_file_b
     )
 
     task3 = PythonOperator(
+        task_id='load_rdf_gz_c',
+        python_callable=load_rdf_file_c
+    )
+
+    task4 = PythonOperator(
+        task_id='load_rdf_gz_d',
+        python_callable=load_rdf_file_d
+    )
+
+    task5 = PythonOperator(
+        task_id='load_rdf_gz_e',
+        python_callable=load_rdf_file_e
+    )
+
+    task6 = PythonOperator(
         task_id='print_log',
         python_callable=log_print_py
     )
 
-    task1 >> task2 >> task3
+    task1 >> task6
